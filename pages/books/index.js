4d463266c3e78ec04c525f73cf11e7e7d7b5961b
Bhuwan chettri <bhuwanchettri@neputer.com>
2022-11-22 21:51:49 +0545

    book page app

--- pages/books/index.js
+++ pages/books/index.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import { books } from "../../data/utils";
 
 const index = () => {
   return (
-    <div>Love to read books</div>
-  )
-}
+    <div>
+      <h1>book details</h1>
 
-export default index
+      <div className="mx-auto bg-slate-300">
+        {books?.map((book, id) => {
+          return (
+            <>
+              <div key={id}>
+                <h1 className="text-xl font-bold text-red-700">{book.name}</h1>
+                <h2>{book.description}</h2>
+                <Link href={`books/${book.id}`}>
+                  <button className="bg-gray-700 text-white font-medium p-5">
+                    Link
+                  </button>
+                </Link>
+
+                <hr></hr>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default index;
